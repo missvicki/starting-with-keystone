4ce3327e207a229851f5e3093931b0a8865a474b
Victor Nomwesigwa <nvbethany479@gmail.com>
2019-07-22 18:47:22 +0300

    ft: Set up an express server - add eslint configuration - add babel configurations - set up the express server [Finishes ft]

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import debug from "debug";
+import chalk from "chalk";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  return res.send("The API is working");
+});
+
+app.listen(port, () => {
+  debug(`Server is running on port ${chalk.green(port)}`);
+});
